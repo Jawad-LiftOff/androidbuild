79adae04dd8249d284d9b777b400bff8d464e717
Jawad-LiftOff <jawad@liftoffllc.com>
2015-08-13 09:28:34 +0530

    removed apk's

--- AppMe/bin/MainActivity-debug-unaligned.apk.d
+++
@@ -1,6 +0,0 @@
-/home/jawad/Android/AppMe/bin/MainActivity-debug-unaligned.apk : \
-/home/jawad/Android/AppMe/bin/MainActivity.ap_ \
-/home/jawad/Android/AppMe/bin/classes.dex \
-/home/jawad/Android/appcompat_v7/bin/classes.jar \
-/home/jawad/Android/appcompat_v7/libs/android-support-v7-appcompat.jar \
-/home/jawad/Android/appcompat_v7/libs/android-support-v4.jar \
